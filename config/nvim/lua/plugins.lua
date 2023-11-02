3a0e1061a02263db904dcdc167e4df169075e31a
khassanove <53072106+khassanove@users.noreply.github.com>
2023-11-02 20:05:36 +0300

    Add files via upload
    nvim

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,83 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+-- Install your plugins here
+return packer.startup(function(use)
+	use ("wbthomason/packer.nvim") -- Have packer manage itself	
+	use "folke/tokyonight.nvim"
+	use 'feline-nvim/feline.nvim'
+	use 'nvim-tree/nvim-web-devicons'
+	
+	use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+
+		
+  
+  	use({
+  "giusgad/pets.nvim",
+  requires = {
+    "giusgad/hologram.nvim",
+    "MunifTanjim/nui.nvim",
+  }
+})
+
+       --alpha nvim
+use {
+    'goolord/alpha-nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+}
+
+
+
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
